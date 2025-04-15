6269084b64a34e309c5de8c91b7ae9ab405ceb5f
Your Name <you@example.com>
2025-04-15 16:30:11 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -107,23 +107,26 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) { 
-    if(map == NULL || map->capacity == 0) return NULL;
-    for(size_t i = 0; i < map->capacity; i++){ //recorre la tabla
-        if(map->buckets[i] != NULL){ //si encuentra un par
-            map->current = i; //actualiza el indice del ultimo dato accedido
-            return map->buckets[i]; //devuelve el par encontrado
-        }
-    }
-    return NULL; //si no se encuentra ningun par, devuelve NULL
-}
+    if (map == NULL || map->capacity == 0) return NULL;
 
-Pair * nextMap(HashMap * map) {
-    if(map == NULL || map->capacity == 0) return NULL;
-    for(long i = map->current + 1; i < map->capacity; ++i){
-        if(map->buckets[i] != NULL){
-            map->current = i; //actualiza el indice del ultimo dato accedido
-            return map->buckets[i]; //devuelve el par encontrado
+    for (size_t i = 0; i < map->capacity; i++) {
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+            map->current = i;
+            return map->buckets[i];
         }
     }
     return NULL;
 }
+
+Pair * nextMap(HashMap * map) {
+    if (map == NULL || map->capacity == 0) return NULL;
+
+    for (long i = map->current + 1; i < map->capacity; ++i) {
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+            map->current = i;
+            return map->buckets[i];
+        }
+    }
+    return NULL;
+}
+
