351488da183c9ef2ba671d6efe7d0f761eaab262
Your Name <you@example.com>
2025-04-15 16:19:35 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -108,7 +108,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) { 
     if(map == NULL) return NULL;
-    for(long i = 0; i < map->capacity; i++){ //recorre la tabla
+    for(size_t i = 0; i < map->capacity; i++){ //recorre la tabla
         if(map->buckets[i] != NULL){ //si encuentra un par
             map->current = i; //actualiza el indice del ultimo dato accedido
             return map->buckets[i]; //devuelve el par encontrado
@@ -119,6 +119,11 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
     if(map == NULL) return NULL;
-    
+    for(long i = map->current; i < map->capacity; ++i){
+        if(map->buckets[i] != NULL){
+            map->current = i; //actualiza el indice del ultimo dato accedido
+            return map->buckets[i]; //devuelve el par encontrado
+        }
+    }
     return NULL;
 }
